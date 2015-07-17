4ed50c0a876493c68e5e09bc6a8f6cd2aa23241f
bysj_team <bysj_team@sohu.com>
2015-07-17 07:45:56 +0000

    five test

--- db/create_users.js
+++ db/create_users.js
@@ -15,13 +15,7 @@ User.sync({force: true}).then(function () {
     },
     {
       numid: '12115011044', 
-      name: '许曼琪', 
-      pass: '123456',
-      unitId: 11
-    },
-    {
-      numid: '12115011044', 
-      name: '李华鸿', 
+      name: '李业富', 
       pass: '123456',
       unitId: 11
     }
